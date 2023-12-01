9d4cc87de60deb3a32d04315e50bcfb21213b251
Cameron <Alleyria@gmail.com>
2023-12-01 15:59:18 +0100

    Cleanup

--- 1.rb
+++ 1.rb
@@ -1,11 +1,24 @@
-puts "Part one: #{File.readlines("1.input").sum(&-> { (_1.match(/(\d)/)[1] + _1.reverse.match(/(\d)/)[1]).to_i })}"
+NUMBERS   =  {
+  "one"   => 1,
+  "two"   => 2,
+  "three" => 3,
+  "four"  => 4,
+  "five"  => 5,
+  "six"   => 6,
+  "seven" => 7,
+  "eight" => 8,
+  "nine"  => 9
+}
 
-def parse_line(input)
-  numbers = { "one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9 }
-  pattern = %r{(?=(\d|#{numbers.keys.join("|")}))}
-  values  = input.strip.scan(pattern).flatten.map { numbers.fetch(_1, _1) }
+def part_one(input)
+  (input.match(/(\d)/)[1] + input.reverse.match(/(\d)/)[1]).to_i
+end
 
+def part_two(input)
+  pattern = %r{(?=(\d|#{NUMBERS.keys.join("|")}))}
+  values  = input.strip.scan(pattern).flatten.map { NUMBERS.fetch(_1, _1) }
   "#{values.first}#{values.last}".to_i
 end
 
-puts "Part two: #{File.readlines("1.input").sum(&method(:parse_line))}"
+puts "Part one: #{File.readlines("1.input").sum(&method(:part_one))}"
+puts "Part two: #{File.readlines("1.input").sum(&method(:part_two))}"
